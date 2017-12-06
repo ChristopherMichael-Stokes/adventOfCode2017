f0620f25eb168e81f0671a2f4b0c9c015a1217c2
Christopher.Michael-Stokes <christophermichaelstokes@gmail.com>
2017-12-06 18:56:34 +0000

    finish part 2

--- day6.cc
+++ day6.cc
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <memory>
+#include <list>
 
 void read_input(std::istream &is, std::vector<int> & inputs)
 {
@@ -9,7 +11,14 @@ void read_input(std::istream &is, std::vector<int> & inputs)
 		inputs.push_back(i);
 }
 
-int part1(std::vector<int> inputs)
+void print_vector(const std::vector<int> & v)
+{
+	for (int i : v)
+		std::cout << i << '\t';
+	std::cout << '\n';
+}
+
+int part1(std::vector<int> & inputs)
 {
 	std::vector<std::vector<int>> before;
 	int cycles = 0;
@@ -20,12 +29,29 @@ int part1(std::vector<int> inputs)
 		for (int i = 0 ; i < inputs.size(); ++i)
 			if (inputs[i] > inputs[idx])
 				idx = i;
-
 		int blocks = inputs[idx];
 		inputs[idx] = 0;
-		for (int i = idx; blocks > 0; --blocks)
+		for (int i = idx+1; blocks > 0; --blocks, ++i)
 			++inputs[i%inputs.size()];
 	} while (std::find(before.cbegin(), before.cend(), inputs) == before.cend());
+	return cycles;
+}
+
+int part2(std::vector<int> inputs)
+{
+	const std::vector<int> state = inputs;
+	int cycles = 0;
+	do {
+		++cycles;
+		int idx =  0;
+		for (int i = 0 ; i < inputs.size(); ++i)
+			if (inputs[i] > inputs[idx])
+				idx = i;
+		int blocks = inputs[idx];
+		inputs[idx] = 0;
+		for (int i = idx+1; blocks > 0; --blocks, ++i)
+			++inputs[i%inputs.size()];
+	} while (state != inputs);
 	return cycles;
 }
 
@@ -34,6 +60,6 @@ int main()
 	std::vector<int> inputs;
 	std::ifstream file("day6input.txt");
 	read_input(file, inputs);
-	std::cout << "part1: " << part1(inputs) << '\n';
+	std::cout << "part1: " << part1(inputs) << " part2: " << part2(inputs) << '\n';
 	return 0;
 }
